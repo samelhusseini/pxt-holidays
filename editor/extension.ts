8ed7ed7ca3b39162547f32e7dcb9540de3709d4f
Sam El-Husseini <samelh@microsoft.com>
2017-11-16 14:46:57 -0800

    Initial pxt-sample check in

---
+++ editor/extension.ts
@@ -0,0 +1,7 @@
+/// <reference path="../node_modules/pxt-core/built/pxteditor.d.ts" />
+
+pxt.editor.initExtensionsAsync = function(opts: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
+  const res: pxt.editor.ExtensionResult = {
+  };
+  return Promise.resolve<pxt.editor.ExtensionResult>(res);
+};
