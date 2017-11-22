711c4eb759b0115a924092c6d08eedd8dbb3560f
Sam El-Husseini <samelh@microsoft.com>
2017-11-21 21:18:58 -0800

    Auto-push

--- site/main_bundle.js
+++ site/main_bundle.js
@@ -19723,13 +19723,7 @@ var MainApp = /** @class */ (function (_super) {
             "public": false,
             "files": this.projects[0].text
         };
-        var headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-GitHub-OTP, X-Requested-With',
-            'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
-            'Access-Control-Expose-Headers': 'ETag, Link, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval',
-            'Access-Control-Max-Age': 86400
-        };
+        var headers = {};
         var url = "https://api.github.com/gists";
         fetch(url, {
             method: 'POST',
